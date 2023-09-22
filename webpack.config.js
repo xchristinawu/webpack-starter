8dc5f6d53a8f35fe3453577b4d2fa716a31f860f
Christina Wu <xchristinawu@gmail.com>
2023-09-21 18:37:27 -0700

    asset resource loader

--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,7 @@ module.exports = {
         path: path.resolve(__dirname, 'dist'),
         filename: '[name][contenthash].js',
         clean: true,
+        assetModuleFilename: '[name][ext]'
     },
     devtool: 'source-map',
     devServer: {
@@ -37,6 +38,10 @@ module.exports = {
                         presets: ['@babel/preset-env']
                     }
                 }
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource'
             }
         ]
     },
